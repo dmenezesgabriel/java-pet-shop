0fe4f5e1f74b746480070a2032738d5aff030019
Gabriel <dmenezes.gabriel@gmail.com>
2021-08-26 00:43:09 -0300

    Add class constructor

--- src/com/petshop/Animal.java
+++ src/com/petshop/Animal.java
@@ -7,12 +7,22 @@ public class Animal {
     String race;
     String coloring;
     double age;
+
+    // Constructors or initialization methods
+    public Animal(String name, String species, String race, String coloring, double age) {
+        this.name = name;
+        this.species = species;
+        this.race = race;
+        this.coloring = coloring;
+        this.age = age;
+    }
+
     // Methods
     void scheduleProcedure(String date, String observation) {
-        System.out.println(String.format("Pet %s agendou procedimento em %s com a observação %s.", name, date , observation));
+        System.out.println(String.format("Pet %s scheduled procedure at %s with description: %s.", name, date , observation));
     }
 
     void finishStay(String date, String observation) {
-        System.out.println(String.format("Pet %s agendou procedimento em %s com a observação %s.", name, date , observation));
+        System.out.println(String.format("Pet %s scheduled procedure at %s with description: %s.", name, date , observation));
     }
 }
--- src/com/petshop/Main.java
+++ src/com/petshop/Main.java
@@ -3,16 +3,10 @@ import com.petshop.Animal;
 
 public class Main {
 
-    static Animal dog = new Animal();
+    static Animal dog = new Animal("Bella", "Dog", "Beagle", "Brown", 0.5);
 
     public static void main(String[] args) {
-        dog.name = "Bella";
-        dog.species = "Cahorro";
-        dog.race = "Beagle";
-        dog.coloring = "Marrom e Branco";
-        dog.age = 0.5;
-
-        dog.scheduleProcedure("02/09/21", "Cortar as unhas");
-        dog.finishStay("03/09/21", "Acrescentar tosa");
+        dog.scheduleProcedure("02/09/21", "Cut nails");
+        dog.finishStay("03/09/21", "Cut hair");
     }
 }
